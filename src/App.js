1df6bc27d6a475aedb075f6f15113fa4b34ed15d
SHIVAM DESHWAL <shivamdeshwal1414@gmail.com>
2023-04-15 10:49:37 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import{useState,useEffect} from "react"
 import './App.css';
+const App=()=>{
+  const APP_ID='e8a6f00f'
+  const APP_KEY='9f6ec7fa6e2c14a728a2bfd5de4d3964	'
+  const[search,setSearch]=useState('momo')
+  const[query,setQuery]=useState('')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+useEffect(()=>{
+  async function getData(){
+    const res=await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response=await res.json()
+    console.log(response); 
+  } 
+  console.log("END");
+  getData()
+},[query])
+const submitHandler=e=>{
+  e.preventDefault()
+  setQuery(search)
+  setSearch('')
+}
+return<>
+<form onSubmit={submitHandler}>
+  {console.log("UI")}
+  <input type="text"onChange={e=>setSearch(e.target.value)}/>
+<button type="submit">Search</button>
+</form>
+</>
 }
 
 export default App;
